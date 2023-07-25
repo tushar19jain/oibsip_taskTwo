66623ef8cdbb726779faef37e7ed283570e8f8c9
tushar19jain <jaint381@gmail.com>
2023-07-25 22:21:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let changingText = document.getElementById('changingText')
+var typed = new Typed(changingText, {
+    strings: ["Full Stack Developer", "Java Developer","C++ Developer","C Developer"],
+    typeSpeed: 40,
+    loop: true
+  });
+  
